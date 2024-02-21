9fa02957d50eefeb314b4612cc63866960a87ffd
Satyajit Kuila <sanu123j@gmail.com>
2024-02-21 09:52:38 +0530

    Session 2

---
+++ LearnAutomationUdemy/SeleniumLearning/SeleniumFirst.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumLearning
+{
+    public class SeleniumFirst
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver=new ChromeDriver();
+
+        }
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://rahulshettyacademy.com/";
+        }
+    }
+}
