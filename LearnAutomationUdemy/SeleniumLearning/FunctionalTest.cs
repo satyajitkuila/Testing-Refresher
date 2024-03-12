0ae60a67138e9d56844f2667e66a3ed673282388
Satyajit Kuila <sanu123j@gmail.com>
2024-03-12 10:57:53 +0530

    12 march session

---
+++ LearnAutomationUdemy/SeleniumLearning/FunctionalTest.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumLearning
+{
+    internal class FunctionalTest
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            //using implicitwait here
+            //driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(5);    
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+            TestContext.Progress.WriteLine("Url :" + driver.Url);
+            TestContext.Progress.WriteLine("Title :" + driver.Title);
+
+        }
+        [Test]
+        public void dropdown()
+        {
+            IWebElement dropdown = driver.FindElement(By.CssSelector("select.form-control"));
+            SelectElement s = new SelectElement(dropdown);
+            s.SelectByText("Teacher");
+            s.SelectByValue("consult");
+            s.SelectByIndex(1);
+
+            //css- tagname[attribute=value]
+            //xpath- //tagname[@attribute=value]
+            IList <IWebElement> rdos = driver.FindElements(By.CssSelector("input[type='radio']"));
+            foreach (IWebElement radiobuttton in rdos)
+            {
+                if (radiobuttton.GetAttribute("value").Equals("user"))
+                {
+                    radiobuttton.Click();
+                }
+            }
+        }
+    }
+}
--- LearnAutomationUdemy/SeleniumLearning/Locators.cs
+++ LearnAutomationUdemy/SeleniumLearning/Locators.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumLearning
 {
@@ -17,6 +18,8 @@ namespace SeleniumLearning
         {
             new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
             driver = new ChromeDriver();
+            //using implicitwait here
+            //driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(5);    
             driver.Manage().Window.Maximize();
             driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
             TestContext.Progress.WriteLine("Url :" + driver.Url);
@@ -30,9 +33,26 @@ namespace SeleniumLearning
             driver.FindElement(By.Id("username")).Clear();
             driver.FindElement(By.Id("username")).SendKeys("rahulshettyy");
             driver.FindElement(By.Name("password")).SendKeys("123456");
-            driver.FindElement(By.CssSelector("input[value='Sign In']")).Click();
+            //driver.FindElement(By.CssSelector("input[value='Sign In']")).Click();
             // --//tagname[@attribute='value']--
+            driver.FindElement(By.XPath("//div[@class='form-group'][5]/label/span/input")).Click();
+            driver.FindElement(By.XPath("//input[@value='Sign In']")).Click();
+            //Thread.Sleep(3000);
 
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(8));
+            //using explicit wait here
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions
+                .TextToBePresentInElementValue(driver.FindElement(By.Id("signInBtn")), "Sign In"));
+            
+            String errorMessage =driver.FindElement(By.ClassName("alert-danger")).Text;
+            TestContext.Progress.WriteLine(errorMessage);
+
+            IWebElement link= driver.FindElement(By.LinkText("Free Access to InterviewQues/ResumeAssistance/Material"));
+            String hrefAttr =link.GetAttribute("href");
+            String expectedUrl = "https://rahulshettyacademy.com/documents-request";
+
+            //Validate URL of the Linktext
+            Assert.AreEqual(expectedUrl, hrefAttr);
 
         }
     }
